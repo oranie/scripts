3ff0cb12a785a39e38396d05eaa837e9d70d1492
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2012-07-01 00:45:45 +0900

    add live_check.sh

---
+++ live_check.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# This script discover alive hosts using GNU Parallel
+#
+
+segment=$1
+
+fnum=`echo $segment | awk -F\. '{print NF}'`
+
+if [ $fnum -ne 3 ]; then
+    echo "ERROR"
+    echo "usage: sh live_check.sh xx.xx.xx"
+    echo "ex): live_check.sh 192.168.1"
+    echo "     Check 192.168.1.1 - 192.168.1.254 are dead or alive"
+    exit 1
+fi
+
+cmd_gnu_parallel="/usr/local/parallel/bin/parallel"
+
+ping_ok_list=`${cmd_gnu_parallel} -j 40 -k "ping -w 1 -c 1 {1} | tr -d '\n' | sed -e 's/$/\n/' | grep -v '100% packet loss'" ::: $(seq 1 254 | sed -e "s%^%${segment}.%") | awk '{print $2}'`
+
+for ip in ${ping_ok_list}; do
+    echo ${ip}
+done
+
